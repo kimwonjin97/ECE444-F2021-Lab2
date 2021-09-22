f7bd08788beba3735ff450155d4e8c1792bbac08
kimwonjin97 <wonjin.kim@mail.utoronto.ca>
2021-09-22 13:32:08 -0400

    Implemented field for email address (Activity 4)

--- hello.py
+++ hello.py
@@ -3,7 +3,10 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
+
+
 
 from datetime import datetime
 
@@ -15,6 +18,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your U of T Email address', validators=[Email()])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -31,11 +35,22 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+            # domain = form.email.data.split('@')[1]
+            
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', 
+                            form=form, 
+                            name=session.get('name'), 
+                            email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
